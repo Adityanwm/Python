c3b74caa14861250d4b2e74db80e5248f439cee4
Adityanwm <adityakumar0708@gmail.com>
2024-05-23 10:35:32 -0500

    Create Day1.py
    Day 1 Practice

---
+++ Day1.py
@@ -0,0 +1,28 @@
+# Day 1 Python
+print('hello')
+
+# variables
+number_of_apples = 5
+print(number_of_apples)
+
+name_of_person = 'Aditya'
+print(name_of_person)
+
+
+#  Aditya kumar
+# name number extr
+
+#Type casting
+
+y = str(6)
+
+y
+
+print(type(y))
+
+x, y = 5 , 6
+x
+s = "this is my name"
+age = 28
+
+print(s + str(age))
